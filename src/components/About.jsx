713ec31187403d3cfd995b571805aa18f789efbf
Jaganravichandran <jaganravichandran1@gmail.com>
2024-12-25 23:34:07 +0530

    episode-08 lifecycle order

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,15 +1,27 @@
-import React from "react";
+import { Component } from "react";
 import User from "./User";
 import UserClass from "./UserClass";
 
-const About = () => {
-  return (
-    <div>
-      <h1>About</h1>
-      <User name={"Jagan Ravichandran"} />
-      <UserClass name={"Jagan Vasanth"} location={"Coimbatore"} />
-    </div>
-  );
-};
+class About extends Component {
+  constructor(props) {
+    super(props);
+
+    console.log("Parent Constructor");
+  }
+
+  componentDidMount() {
+    console.log("Parent Component Did Mount");
+  }
+  render() {
+    console.log("Parent Render");
+
+    return (
+      <div>
+        <h1>About</h1>
+        <UserClass name={"Jagan Vasanth"} location={"Coimbatore"} />
+      </div>
+    );
+  }
+}
 
 export default About;
--- src/components/User.jsx
+++ src/components/User.jsx
@@ -2,11 +2,23 @@ import React, { useState } from "react";
 
 const User = ({ name }) => {
   const [count, setCount] = useState(0);
-  const [count2, setCount2] = useState(1);
   return (
     <div className="user-card">
       <h1>Count : {count}</h1>
-      <h1>Count2 : {count2}</h1>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Count Increase
+      </button>
+      <button
+        onClick={() => {
+          setCount(count > 0 ? count - 1 : count);
+        }}
+      >
+        Count Decrease
+      </button>
       <h2>Name : {name}</h2>
       <h3>Location : Tamil Nadu</h3>
     </div>
