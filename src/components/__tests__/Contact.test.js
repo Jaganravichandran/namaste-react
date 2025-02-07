6e7099e3af3eacea4ef8f4c072780579d3faf8f5
Jaganravichandran <jaganravichandran1@gmail.com>
2025-02-07 13:58:20 +0530

    episode-13 part-04

--- src/components/__tests__/Contact.test.js
+++ src/components/__tests__/Contact.test.js
@@ -4,9 +4,29 @@ import "@testing-library/jest-dom";
 
 test("should load the contact us component", () => {
   render(<Contact />);
-
+  // Querying
   const heading = screen.getByRole("heading");
 
   // Assertion
   expect(heading).toBeInTheDocument();
+});
+
+test("should load button inside Contact component", () => {
+  render(<Contact />);
+
+  // const button = screen.getByRole("button");
+  const button = screen.getByText("Submit");
+
+  // Assertion
+  expect(button).toBeInTheDocument();
+});
+
+test("should load 2 input boxes in the Contact component", () => {
+  render(<Contact />);
+
+  const inputBoxes = screen.getAllByRole("textbox");
+  // console.log(inputBoxes.length);
+
+  // Assertion
+  expect(inputBoxes.length).toBe(2);
 });
